ca71d89104e9d7aad020e08101def55b337abf52
DPach <63697168+Prads-ai@users.noreply.github.com>
2023-09-19 13:36:04 -0400

    quizz_brain.py
    Quizz manager

---
+++ 100_days_Angela_Yu_Projects/Day-17/Quiz_Game/quizz_brain.py
@@ -0,0 +1,29 @@
+class QuizBrain:
+    def __init__(self, q_list):
+        self.question_number = 0
+        self.question_list = q_list
+        self.score = 0
+
+    def still_has_question(self):
+        return len(self.question_list) - self.question_number
+
+    def next_question(self):
+        current_question = self.question_list[self.question_number]
+        self.question_number += 1
+        user_answer = input(f"Q.{self.question_number}: {current_question.text} (True | False): ")
+        self.check_answer(user_answer, current_question.answer)
+
+    def check_answer(self, user_answer, correct_answer):
+        if user_answer == correct_answer.lower():
+            print(f"you got it right")
+            self.score += 1
+
+        else:
+            print(f"you got it wrong")
+        print(f"the correct answer was: {correct_answer}")
+        print(f"your current score is : {self.score} / {self.question_number}")
+        print('\n')
+
+    def report(self):
+        print(f"you've completed the quizz")
+        print(f"your final score was: {self.score} / {len(self.question_list)}")
