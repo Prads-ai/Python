f188a1f11e007686ebc5ef464091f22e1297141f
DPach <63697168+Prads-ai@users.noreply.github.com>
2023-09-19 13:35:26 -0400

    question_model.py

---
+++ 100_days_Angela_Yu_Projects/Day-17/Quiz_Game/question_model.py
@@ -0,0 +1,7 @@
+# TODO Create a question class with a constructor that will initialize two attribute : text and the answer
+class Question:
+    def __init__(self, q_text, q_answer):
+        self.text = q_text
+        self.answer = q_answer
+
+
